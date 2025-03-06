e7b20f0c3997670875baac37c121dec149de9870
David Casanova <casanova.83130@gmail.com>
2025-03-06 16:55:48 +0100

    design ok

--- public/js/main.js
+++ public/js/main.js
@@ -58,79 +58,84 @@ faqItems.forEach((item) => {
 });
 
 
-// //scroll reveal animations
-// const sr = ScrollReveal({
-//   origin: 'bottom',
-//   distance: '60px',
-//   duration : 3000,
-//   delay: 400,
-//   //reset: true // resets animation
-// });
+//scroll reveal animations
+const sr = ScrollReveal({
+  origin: 'bottom',
+  distance: '60px',
+  duration : 3000,
+  delay: 400,
+  //reset: true // resets animation
+});
 
-// //hero
-// sr.reveal('.hero__text', { origin: 'top'});
-// sr.reveal('.hero__img');
-
-// //stats
-// sr.reveal('.stats__item', { delay: 600, distance: '100px', interval: 100, origin: 'top'});
-
-// // services
-// sr.reveal('.services');
-// sr.reveal('.services__top');
-// sr.reveal('.services__item', {
-//   delay: 600,
-//   distance :'100px',
-//   interval:100,
-//   origin:'top'
-// });
-
-// // appointment
-// sr.reveal('.appointment__title');
-// sr.reveal('.appointment__form');
-
-// // testimonials
-// sr.reveal('.testimonials');
-// sr.reveal('.testimonials_container');
-
-// // team
-// sr.reveal('.team__title');
-// sr.reveal('.team__slider');
-
-// // faq
-// sr.reveal('.faq__title');
-// sr.reveal('.faq__item',{
-//   delay: 600,
-//   distance :'100px',
-//   interval:100,
-//   origin:'top'
-// });
-
-// // departments
-// sr.reveal('.departments__bg');
-// sr.reveal('.departments__container');
+//hero
+sr.reveal('.hero__text', { origin: 'top',delay: 50,  duration : 3000});
+sr.reveal('.hero__img');
 
 
-// // blog
-// sr.reveal('.blog__title');
-// sr.reveal('.blog__post',{
-//   delay: 600,
-//   distance :'100px',
-//   interval:100,
-//   origin:'top'
-// });
-
-// // brands
-// sr.reveal('.brands__logo',{
-//   delay: 600,
-//   distance :'100px',
-//   interval:100,
-//   origin:'top'
-// });
+// //slider photo home
+// sr.reveal('.slider__photo');
 
 
-// // newsletter
-// sr.reveal('.newsletter');
-// sr.reveal('.newsletter__container');
+//stats
+sr.reveal('.stats__item', { delay: 600, distance: '100px', interval: 100, origin: 'top'});
+
+// services
+sr.reveal('.services');
+sr.reveal('.services__top');
+sr.reveal('.services__item', {
+  delay: 600,
+  distance :'100px',
+  interval:100,
+  origin:'top'
+});
+
+// appointment
+sr.reveal('.appointment__title', { origin: 'top',delay: 50,  duration : 3000});
+sr.reveal('.appointment__form');
+
+// testimonials
+sr.reveal('.testimonials');
+sr.reveal('.testimonials_container');
+
+// team
+sr.reveal('.team__title');
+sr.reveal('.team__slider');
+
+// faq
+sr.reveal('.faq__title');
+sr.reveal('.faq__item',{
+  delay: 600,
+  distance :'100px',
+  interval:100,
+  origin:'top'
+});
+
+// departments
+sr.reveal('.departments__bg');
+sr.reveal('.departments__container');
+
+
+// blog
+sr.reveal('.blog__title');
+sr.reveal('.blog__post',{
+  delay: 600,
+  distance :'100px',
+  interval:100,
+  origin:'top'
+});
+
+// brands
+sr.reveal('.brands__logo',{
+  delay: 600,
+  distance :'100px',
+  interval:100,
+  origin:'top'
+});
+
+
+// newsletter
+sr.reveal('.newsletter');
+sr.reveal('.newsletter__container');
 
 // // footer
 
